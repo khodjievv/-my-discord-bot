9788b5c99307856d756fad61bf6738b310c4fe22
X <xodjiyevfirdavs0@gmail.com>
2026-07-28 20:13:24 +0500

    Make interaction reply ephemeral
    Changed interaction reply to be ephemeral, hiding command usage from public chat.

--- index.js
+++ index.js
@@ -718,7 +718,8 @@ client.on('interactionCreate', async interaction => {
                 .setEmoji('📥')
         );
 
-      await interaction.reply({ embeds: [embed], components: [row] });
+      // Using ephemeral: true so the command response doesn't show "[User] used /ticketpanel" publicly in chat
+      await interaction.reply({ embeds: [embed], components: [row], ephemeral: true });
     }
 
     else if (commandName === 'say') {
